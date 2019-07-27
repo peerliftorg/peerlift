e397ed9bee46d04412f1796e21589fdf822cb276
samjgorman <samjgorman@gmail.com>
2019-07-27 15:44:44 -0700

    setting up db

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../containers/App.css';
 import MyNavbar from '../components/nav.js';
 import AddOpp from '../components/addOpp.js';
 import OppBox from '../components/OppBox';
 
 
-function App() {
+class App extends Component {
+  state = {users: []}
+  componentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+  render() {
   return (
 
     <div className = 'wrapper'> 
         <MyNavbar> </MyNavbar>
         <AddOpp></AddOpp>
         <OppBox></OppBox>
+        
+        {this.state.users.map(user =>
+          <div key={user.id}>{user.username}</div>
+          )}
         </div>
-
+    
   );
-
+}
 }
 
 export default App;
