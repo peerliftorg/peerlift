b62f99648abbbe44e949476470c924d50944be29
samjgorman <samjgorman@gmail.com>
2019-07-31 21:39:13 -0700

    we love mongodb

--- react-backend/app.js
+++ react-backend/app.js
@@ -10,25 +10,52 @@ var app = express();
 app.use(cors());
 
 
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//Successful connect of classic Mongo DB
+// const MongoClient = require('mongodb').MongoClient;
+// const ObjectId = require("mongodb").ObjectID;
+
+// const uri = "mongodb+srv://peerlift:atheno75@pl-connect-0azku.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   //does not show if db is correct yet
+//   const collection = client.db("sample_airbnb").collection("listingAndReviews");
+//   if(err) {
+//     console.log('The database is off chief');
+// }
+// console.log('The database works chief');
+
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://peerlift:atheno75@pl-connect-0azku.mongodb.net/test?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
 
 // //This creates a connection to a local db
-const mysql = require('mysql');
+//const mysql = require('mysql');
 
 //connect to users from tutorial
-app.get('/user/', (req, res) => {
-  console.log("Fetching user data ")
+// app.get('/user/', (req, res) => {
+//   console.log("Fetching user data ")
 
-  //auth info to connect to a local db
-  // const connection = mysql.createConnection({
-  //   host: 'localhost',
-  //   user: 'root',
-  //   password: 'runfast433',
-  //   database: 'sys'
-  // })
+//   //auth info to connect to a local db
+//   const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'PASSWORD',
+//     database: 'pl_connect'
+//   })
 
   // //auth info to connect to aws rds, not yet working
   // const connection = mysql.createConnection({
@@ -41,13 +68,13 @@ app.get('/user/', (req, res) => {
   // })
 
   //with new db
-  const connection = mysql.createConnection({
-    host: 'aacjik3kzvv0hg.cwp44tiqr9lo.us-east-2.rds.amazonaws.com',
-    port: '3306',
-    user: 'plconnect',
-    password: 'atheno75!',
-    database: 'ebdb'
-  });
+  // const connection = mysql.createConnection({
+  //   host: 'aacjik3kzvv0hg.cwp44tiqr9lo.us-east-2.rds.amazonaws.com',
+  //   port: '3306',
+  //   user: 'plconnect',
+  //   password: 'atheno75!',
+  //   database: 'ebdb'
+  // });
 
 
 //set up to connect to amazon rds db 
@@ -58,46 +85,47 @@ app.get('/user/', (req, res) => {
 //   user     : process.env.RDS_USERNAME,
 //   password : process.env.RDS_PASSWORD,
 //   port     : process.env.RDS_PORT,
+  
 //   //socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 
 // });
 
 
 
-  connection.connect(function(err){
+//   connection.connect(function(err){
 
-    if(!err) {
-        console.log("Database is connected ... ");    
-    } else {
-        console.log("Error connecting database ... " + err);    
-    }
-    });
+//     if(!err) {
+//         console.log("Database is connected ... ");    
+//     } else {
+//         console.log("Error connecting database ... " + err);    
+//     }
+//     });
   
 
-  //const userId = req.params.id
+//   //const userId = req.params.id
 
 
-  //code to query local db
-  const queryString = "SELECT * FROM test"
-  connection.query(queryString, (err, rows, fields) => {
-    if (err) {
-      console.log("Failed to query for users: " + err)
-      res.sendStatus(500)
-      return
-      // throw err
-    }
+//   //code to query local db
+//   const queryString = "SELECT * FROM scholarships"
+//   connection.query(queryString, (err, rows, fields) => {
+//     if (err) {
+//       console.log("Failed to query for users: " + err)
+//       res.sendStatus(500)
+//       return
+//       // throw err
+//     }
 
-    console.log("I think we fetched users successfully")
+//     console.log("I think we fetched users successfully")
 
-    const users = rows.map((row) => {
-      return {title: row.title, deadline: row.deadline}
-    })
+//     const users = rows.map((row) => {
+//       return {title: row.title, deadline: row.deadline}
+//     })
 
-    res.json(users)
-  })
+//     res.json(users)
+//   })
 
-   //res.end()
-})
+//    //res.end()
+// })
 
 
 // const con = mysql.createConnection({
@@ -156,16 +184,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-
-
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error('Database connection failed: ' + err.stack);
-//     return;
-//   }
-
-//   console.log('Connected to database.');
-// });
-
-// connection.end();
