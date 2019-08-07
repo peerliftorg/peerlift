ecae965dd6f1c74a581358c1bb26e02145b403a1
samjgorman <samjgorman@gmail.com>
2019-08-07 04:52:31 -0700

    post-request-works

--- client/src/components/addOpp.js
+++ client/src/components/addOpp.js
@@ -8,14 +8,46 @@ import '../components/addOpp.css';
 //Make long for add button
 
 class AddOpp extends Component{
+
+    constructor(props){
+        super(props);
+            this.state = {
+                name: ""
+            };
+        }
+    changeHandler = e => {
+        this.setState({name: e.target.value});
+    }
+
+    submitHandler = e => {
+        e.preventDefault()
+        console.log("should input test data" +this.state)
+        fetch('http://localhost:9000/users', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "Name": this.state.name
+            })
+            })
+    }
+    
+    //process post request
+    // myClick(){
+     
+    // }
+
     render() {
+        const {name} = this.state
         return (
             <div className = "FormContainer"> 
             <div className = "OppText">🤑 Add a scholarship </div>
-            <form className = "Form">
-                <input className = "FormInput" placeholder = "What's it called?">
+            <form onSubmit= {this.submitHandler} className = "Form">
+                <input className = "FormInput" type = "text" placeholder = "What's it called?" value ={this.state.name} onChange = {this.changeHandler}>
                 </input>
-                <button className = "FormButton"> Add
+                <button type = "submit" className = "FormButton" onClick = {this.myClick}> Add
                 </button>
             </form>
             </div>
