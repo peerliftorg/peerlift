c9f8cb91bf79f36b63934637044c4228a9d6630b
samjgorman <samjgorman@gmail.com>
2019-08-26 16:01:40 -0700

    heroku st up

--- react-backend/server.js
+++ react-backend/server.js
@@ -29,6 +29,16 @@ app.use('/', indexRouter);
 app.use('/scholarships', usersRouter);
 app.use('/summerprograms', summerRouter);
 
+//For production, serve react files statically
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
+    });
+
+}
+
 
 //This code connects our mongodb database to the express server
 const mongoose = require('mongoose');
