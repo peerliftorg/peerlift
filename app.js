1f5e572712f87095cea116deb85f454c9488a605
samjgorman <samjgorman@gmail.com>
2019-08-27 12:35:24 -0700

    test

--- app.js
+++ app.js
@@ -29,16 +29,24 @@ app.use('/', indexRouter);
 app.use('/scholarships', usersRouter);
 app.use('/summerprograms', summerRouter);
 
-//For production, serve react files statically
-if(process.env.NODE_ENV === 'production'){
-    //set static folder
     app.use(express.static('client/build'));
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
-        console.log('loaded prod react');
-    });
 
-}
+    app.get('*', (req,res) => {
+              res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
+              console.log('loaded prod react');
+          });
+
+
+//For production, serve react files statically
+// if(process.env.NODE_ENV === 'production'){
+//     //set static folder
+//     app.use(express.static('client/build'));
+//     app.get('*', (req,res) => {
+//         res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
+//         console.log('loaded prod react');
+//     });
+
+// }
 
 
 //This code connects our mongodb database to the express server
