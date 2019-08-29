adc71da9973a04d930be76382439771913a56527
samjgorman <samjgorman@gmail.com>
2019-08-29 14:44:13 -0700

    basic 404

--- client/src/components/notFound.js
+++ client/src/components/notFound.js
@@ -1,5 +1,38 @@
-import React from 'react'
+import React, {Component} from 'react'; 
+import MyNavbar from '../components/nav.js';
+import Footer from "../components/footer";
+import '../components/notFound.css';
 
 
-const Notfound = () => <h1>Not found</h1>
+
+class Notfound extends Component {
+    //adding constructor from tut
+    constructor(props){
+      super(props);
+      //now assign state, and a default value, doesnt matter if i say data
+      this.state = {
+        //contacts: []
+      };
+    }
+   
+  
+    render() {
+    return (
+      // <Router> 
+      <div className = 'wrapper'> 
+          <MyNavbar> </MyNavbar>
+         
+         <div className = "wrapper-404"> 
+         <div className = "title-404">Oops! 😳</div>
+         <div className = "sub-404">What you’re looking for may have changed, or doesn’t exist on Peerlift.</div>
+         </div>
+
+  
+          <Footer></Footer>
+       
+        </div>
+    );
+  }
+  }
+  
 export default Notfound
