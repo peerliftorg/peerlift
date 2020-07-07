fe642a40fc1e3f30ae7649062a8a64ed94d27913
samjgorman <samjgorman@gmail.com>
2020-07-06 20:04:50 -0700

    prod

--- app.js
+++ app.js
@@ -41,15 +41,15 @@ app.use('/api/email', emailRouter);
 
 
 //For production, serve react files statically
-// if(process.env.NODE_ENV === 'production'){
-//     //set static folder
-//     app.use(express.static('client/build'));
-//     app.get('*', (req,res) => {
-//         res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
-//         console.log('loaded prod react');
-//     });
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
+        console.log('loaded prod react');
+    });
 
-// }
+}
 
 
 //This code connects our mongodb database to the express server
